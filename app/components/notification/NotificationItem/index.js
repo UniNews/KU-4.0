76916fdd0708160836be7cc2803e62a479f719b8
kakmond <wongsathorn.pa@ku.th>
2020-01-30 17:29:09 +0700

    decreased font size

---
+++ app/components/notification/NotificationItem/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import { View, Image, Text, TouchableWithoutFeedback } from 'react-native'
+import styles from './styles'
+import PropTypes from 'prop-types'
+import Hr from '../../commons/Hr'
+import { Feather } from '@expo/vector-icons'
+
+class NotificationItem extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    onNewsPressedHandler = () => {
+        const { onNewsPressed, data } = this.props
+        if (onNewsPressed)
+            onNewsPressed(data.newsId)
+    }
+
+    onProfilePressedHandler = () => {
+        const { onProfilePressed, data } = this.props
+        if (onProfilePressed)
+            onProfilePressed(data.profileId)
+    }
+
+    render() {
+        const { data } = this.props
+        return (
+            <TouchableWithoutFeedback onPress={this.onNewsPressedHandler}>
+                <View>
+                    <View style={styles.container}>
+                        <View style={styles.leftContainer}>
+                            <TouchableWithoutFeedback onPress={this.onProfilePressedHandler}>
+                                <Image
+                                    source={{ uri: data.profileImg }}
+                                    style={styles.avatar}
+                                />
+                            </TouchableWithoutFeedback>
+                            <View>
+                                <TouchableWithoutFeedback onPress={this.onProfilePressedHandler}>
+                                    <Text style={styles.profileNameText}>
+                                        {data.profileName}
+                                    </Text>
+                                </TouchableWithoutFeedback>
+                                <Text style={styles.description}>
+                                    {data.description}
+                                    <Text style={styles.date}>
+                                        {' • ' + data.date}
+                                    </Text>
+                                </Text>
+                            </View>
+                        </View>
+                        <View style={styles.rightContainer}>
+                            <View style={styles.buttonContainer}>
+                                <Feather name={'chevron-right'} size={20} color={'gray'} />
+                            </View>
+                        </View>
+                    </View>
+                    <Hr />
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
+}
+
+NotificationItem.propTypes = {
+    data: PropTypes.shape({
+        profileId: PropTypes.number.isRequired,
+        profileName: PropTypes.string.isRequired,
+        profileImg: PropTypes.string.isRequired,
+        newsId: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+    }).isRequired,
+    onProfilePressed: PropTypes.func,
+    onNewsPressed: PropTypes.func
+}
+
+NotificationItem.defaultProps = {
+    data: {
+        profileId: null,
+        profileName: '',
+        profileImg: '',
+        newsId: null,
+        description: '',
+    },
+}
+
+export default NotificationItem
---
+++ app/components/notification/NotificationItem/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+import { REGULAR_FONT, BOLD_FONT } from '../../../assets/css/typography'
+
+export default StyleSheet.create({
+    container: {
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    leftContainer: {
+        flex: 4,
+        flexDirection: 'row',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    avatar: {
+        alignSelf: 'flex-start',
+        width: 70,
+        height: 70,
+        borderRadius: 70 / 2,
+    },
+    profileNameText: {
+        fontSize: 15,
+        paddingHorizontal: 20,
+        fontFamily: BOLD_FONT
+    },
+    buttonContainer: {
+        alignSelf: 'flex-end',
+    },
+    description: {
+        paddingHorizontal: 20,
+        fontFamily: REGULAR_FONT,
+        fontSize: 14,
+    },
+    date: {
+        color: 'grey',
+    }
+});
