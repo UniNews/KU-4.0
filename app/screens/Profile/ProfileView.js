906cba5d812d469d6550d24a4f3fd753a932b525
kakmond <wongsathorn.pa@ku.th>
2020-01-30 15:31:22 +0700

    added profile page

--- app/screens/Profile/ProfileView.js
+++ app/screens/Profile/ProfileView.js
@@ -1,20 +1,89 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
+import React from 'react'
+import { Text, View, Image, TouchableWithoutFeedback } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import styles from './styles'
+import { LinearGradient } from 'expo-linear-gradient'
+import Hr from '../../components/commons/Hr'
+import Switch from '../../components/commons/Switch'
 
 class ProfileView extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props)
+        this.state = {
+            switchValue: false,
+            language: 'th'
+        }
+    }
+
+    toggleSwitch = (value) => {
+        this.setState({ switchValue: value })
+    }
+
+    changeLanguage = (language) => {
+        this.setState({ language })
     }
 
     render() {
+        const { language } = this.state
         return (
             <View style={styles.containter}>
-                <Text>Profile!</Text>
+                <View style={styles.headContainer}>
+                    <LinearGradient style={styles.linearGradient} start={{ x: 0, y: 0 }} end={{ x: 0, y: 0.8 }} colors={['#465859', '#588E57']}>
+                        <View style={styles.innerHeadContainer}>
+                            <Image
+                                source={{ uri: 'https://scontent.fbkk2-5.fna.fbcdn.net/v/t1.0-1/c0.0.820.820a/66686017_1125283600997160_4542151837934944256_n.jpg?_nc_cat=110&_nc_oc=AQmNpBihC6kLplVjqVJTIrCFMMm59mZi4qLk9VaYlJEaVjaWaGnRNVpvjGEXBl3tINA&_nc_ht=scontent.fbkk2-5.fna&oh=326e11cf10014a155dd953be098951f0&oe=5E90FC5F' }}
+                                style={styles.avatar}
+                            />
+                            <Text style={styles.name}>
+                                Jame Sathira
+                            </Text>
+                            <Text style={styles.faculty}>
+                                ซอฟแวร์ และความรู้
+                            </Text>
+                        </View>
+                    </LinearGradient>
+                </View>
+                <TouchableWithoutFeedback onPress={() => this.props.navigation.navigate('ProfileSetting')}>
+                    <View style={styles.settingContainer}>
+                        <Text style={styles.settingText}>รูป และชื่อผู้ใช้</Text>
+                        <Feather name={'chevron-right'} size={20} color={'gray'} />
+                    </View>
+                </TouchableWithoutFeedback>
+                <Hr />
+                <TouchableWithoutFeedback onPress={() => this.props.navigation.navigate('FollowingSetting')}>
+                    <View style={styles.settingContainer}>
+                        <Text style={styles.settingText}>กำลังติดตาม</Text>
+                        <Feather name={'chevron-right'} size={20} color={'gray'} />
+                    </View>
+                </TouchableWithoutFeedback>
+                <Hr />
+                <View style={styles.settingContainer}>
+
+                    <Text style={styles.settingText}>การแจ้งเตือน</Text>
+                    <Switch
+                        toggleSwitch={this.toggleSwitch}
+                        switchValue={this.state.switchValue} />
+                </View>
+                <Hr />
+                <View style={styles.settingContainer}>
+                    <Text style={styles.settingText}>ภาษา</Text>
+                    <View style={styles.flagContainer}>
+                        <TouchableWithoutFeedback onPress={() => this.changeLanguage('th')}>
+                            <Image style={language == 'th' ? styles.flagImage : [styles.flagImage, styles.isFocused]} source={require('../../assets/imgs/thai.jpg')} />
+                        </TouchableWithoutFeedback>
+                        <TouchableWithoutFeedback onPress={() => this.changeLanguage('eng')}>
+                            <Image style={language == 'eng' ? styles.flagImage : [styles.flagImage, styles.isFocused]} source={require('../../assets/imgs/eng.jpg')} />
+                        </TouchableWithoutFeedback>
+                    </View>
+                </View>
+                <Hr />
+                <View style={styles.settingContainer}>
+                    <Text style={styles.settingText}>ออกจากระบบ</Text>
+                </View>
             </View>
-        );
+        )
     }
 }
 
-export default ProfileView;
+export default ProfileView
--- app/screens/Profile/styles.js
+++ app/screens/Profile/styles.js
@@ -1,10 +1,73 @@
-import { StyleSheet } from 'react-native';
-import Constants from '../../configs/constants';
+import { StyleSheet } from 'react-native'
+import { viewportWidth, wp } from '../../assets/javascripts/spacing'
+import { BOLD_FONT, REGULAR_FONT } from '../../assets/css/typography'
 
+const circleWidth = viewportWidth + wp(50)
 export default StyleSheet.create({
     containter: {
         flex: 1,
-        justifyContent: 'center',
+    },
+    linearGradient: {
+        width: circleWidth,
+        borderBottomWidth: 3,
+        borderEndWidth: 3,
+        borderStartWidth: 3,
+        borderColor: 'white',
+        borderBottomLeftRadius: circleWidth / 2,
+        borderBottomRightRadius: circleWidth / 2,
+        shadowRadius: 2,
+        shadowOffset: {
+            width: 0,
+            height: -3,
+        },
+        shadowColor: 'black',
+        elevation: 5,
+    },
+    innerHeadContainer: {
         alignItems: 'center',
+        paddingTop: 30,
+        paddingBottom: 40
+    },
+    avatar: {
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+        borderWidth: 2,
+        borderColor: 'white',
+        marginBottom: 10
+    },
+    headContainer: {
+        alignItems: 'center',
+    },
+    name: {
+        fontFamily: BOLD_FONT,
+        fontSize: 30,
+        color: 'white',
+    },
+    faculty: {
+        fontFamily: REGULAR_FONT,
+        fontSize: 20,
+        color: 'white',
+    },
+    settingText: {
+        fontFamily: REGULAR_FONT,
+        fontSize: 15,
+    },
+    settingContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20
+    },
+    flagContainer: {
+        flexDirection: 'row'
+    },
+    flagImage: {
+        width: 30,
+        height: 20,
+        borderRadius: 2,
+        marginHorizontal: 5
+    },
+    isFocused: {
+        opacity: 0.2
     }
-});
+})
