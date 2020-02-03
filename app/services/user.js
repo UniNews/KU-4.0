37b011097955e4ce46aa3807fa3f69a09a0be70a
kakmond <wongsathorn.pa@ku.th>
2020-02-03 15:50:40 +0700

    removed async function

--- app/services/user.js
+++ app/services/user.js
@@ -2,13 +2,13 @@ import constants from '../configs/constants';
 import axios from 'axios';
 
 export default {
-    login: async (username, password) => {
+    login: (username, password) => {
         const json = {
             grant_type: 'password',
             username: username,
             password: password
         }
-        return await axios.post(`${constants.API_URL}/token`, json, {
+        return axios.post(`${constants.API_URL}/token`, json, {
             headers: { 'Content-Type': 'application/json' }
         })
             .then(response => response.data)
