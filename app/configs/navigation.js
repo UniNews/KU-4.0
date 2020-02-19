950bfc0191b68a12331f2a09ed8ad1c59bb07336
kakmond <wongsathorn.pa@ku.th>
2020-02-20 00:11:49 +0700

    added community pages to navigation

--- app/configs/navigation.js
+++ app/configs/navigation.js
@@ -5,7 +5,6 @@ import { createStackNavigator } from 'react-navigation-stack'
 import { FontAwesome } from '@expo/vector-icons'
 import NewsTab from '../components/news/NewsTab'
 import NotificationScreen from '../screens/Notification/'
-import ScheduleScreen from '../screens/Schedule/'
 import UserProfileScreen from '../screens/Profile/User'
 import StoreProfileScreen from '../screens/Profile/Store'
 import ClubScreen from '../screens/News/Club/'
@@ -13,12 +12,15 @@ import UniversityScreen from '../screens/News/University/'
 import RecommendationScreen from '../screens/News/Recommendation/'
 import PromotionScreen from '../screens/News/Promotion/'
 import LoginScreen from '../screens/Login/'
-import DetailScreen from '../screens/Detail'
+import NewsDetailScreen from '../screens/Detail'
+import CommunityDetailScreen from '../screens/Community/Detail'
 import { PRIMARY_COLOR, SECONDARY_COLOR } from '../assets/css/color'
 import { BOLD_FONT } from '../assets/css/typography'
 import ProfileSetting from '../screens/Setting/Profile'
 import FollowingSetting from '../screens/Setting/Following'
 import CommentScreen from '../screens/Comment'
+import LatestCommunity from '../screens/Community/Latest'
+import HottestComminity from '../screens/Community/Hottest'
 
 const newsTab = createMaterialTopTabNavigator({
   'สำหรับคุณ': RecommendationScreen,
@@ -35,7 +37,7 @@ const newsTab = createMaterialTopTabNavigator({
 
 const newsStack = createStackNavigator({
   Home: newsTab,
-  Detail: DetailScreen,
+  Detail: NewsDetailScreen,
   Comment: CommentScreen,
   ProfileDetail: StoreProfileScreen
 },
@@ -50,6 +52,38 @@ const newsStack = createStackNavigator({
 newsStack.navigationOptions = ({ navigation }) => {
   let tabBarVisible = true
   if (navigation.state.index == 2) // check if the route is Comment
+    tabBarVisible = false
+  return {
+    tabBarVisible,
+  }
+}
+
+const communityTab = createMaterialTopTabNavigator({
+  'ล่าสุด': LatestCommunity,
+  'กำลังฮิต': HottestComminity,
+}, {
+  tabBarComponent: NewsTab,
+  swipeEnabled: true,
+  tabBarOptions: {
+    scrollEnabled: true,
+  },
+})
+
+const communityStack = createStackNavigator({
+  Home: communityTab,
+  Detail: CommunityDetailScreen,
+},
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    }
+  }
+)
+
+communityStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible = true
+  if (navigation.state.index == 1) // check if the route is Detail
     tabBarVisible = false
   return {
     tabBarVisible,
@@ -72,7 +106,7 @@ const profileStack = createStackNavigator({
 
 const TabNavigator = createBottomTabNavigator({
   'หน้าหลัก': newsStack,
-  'ปฏิทิน': ScheduleScreen,
+  'พูดคุย': communityStack,
   'แจ้งเตือน': NotificationScreen,
   'โปรไฟล์': profileStack
 },
@@ -86,7 +120,7 @@ const TabNavigator = createBottomTabNavigator({
         if (routeName === 'หน้าหลัก') {
           iconName = 'home'
           size = 28
-        } else if (routeName === 'ปฏิทิน') {
+        } else if (routeName === 'พูดคุย') {
           iconName = 'calendar'
           size = 23
         }
