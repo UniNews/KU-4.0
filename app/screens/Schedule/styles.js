e4a791cfd19f78b7d426e94ca62e9a6dd182cc34
kakmond <wongsathorn.pa@ku.th>
2020-02-20 00:12:29 +0700

    deleted the schedule page

--- app/screens/Schedule/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-    containter: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-});
