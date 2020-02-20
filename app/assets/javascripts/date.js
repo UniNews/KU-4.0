259806d0b2598b8835527263e3b0280d838fc41b
wasuthun <pond_zen@hotmail.com>
2020-02-20 10:06:28 +0700

    add format date thai

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,19 +1,16 @@
 var moment = require('moment');
 
-var monthNamesThai = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน",
-    "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤษจิกายน", "ธันวาคม"];
-
-var dayNames = ["วันอาทิตย์ที่", "วันจันทร์ที่", "วันอังคารที่", "วันพุทธที่", "วันพฤหัสบดีที่", "วันศุกร์ที่", "วันเสาร์ที่"];
+var monthNamesThai = ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.",
+    "ก.ค.", "ส.ค.", " ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."]
 
 export function convertTimestamptoDate(timestamp) {
-    const date = new Date(timestamp)
     const moments = new moment(timestamp)
     if(moments.isSame(new Date(),'day')){
         return moments.startOf('hour').fromNow()
     } else {
         return moments.calendar(null, {
-            lastDay: '[เมื่อวาน] HH:mm',
-            sameElse: 'DD MMMM YYYY HH:mm'
+            lastDay: '[เมื่อวาน] HH:mm น.',
+            sameElse: 'DD '+ monthNamesThai[moments.month()]+ ' YYYY | HH:mm น.'
         })
     }
 }
