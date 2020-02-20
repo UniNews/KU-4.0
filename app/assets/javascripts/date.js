cc3033068f5bedf18c4e0f23c50cb0d387af2d89
kakmond <wongsathorn.pa@ku.th>
2020-02-20 16:17:03 +0700

    formatted code

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,16 +1,16 @@
 var moment = require('moment');
 
-var monthNamesThai = ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.",
-    "ก.ค.", "ส.ค.", " ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."]
+var monthNamesThai = ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.',
+    'ก.ค.', 'ส.ค.', ' ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.']
 
 export function convertTimestamptoDate(timestamp) {
     const moments = new moment(timestamp)
-    if(moments.isSame(new Date(),'day')){
+    if (moments.isSame(new Date(), 'day')) {
         return moments.startOf('hour').fromNow()
     } else {
         return moments.calendar(null, {
             lastDay: '[เมื่อวาน] HH:mm น.',
-            sameElse: 'DD '+ monthNamesThai[moments.month()]+ ' YYYY | HH:mm น.'
+            sameElse: 'DD ' + monthNamesThai[moments.month()] + ' YYYY | HH:mm น.'
         })
     }
 }
