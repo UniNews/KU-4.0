3a5b873a2c45af7f83423557e713bcdc2610afc4
kakmond <wongsathorn.pa@ku.th>
2020-02-20 14:56:49 +0700

    changed APP_ID

--- app/configs/constants.js
+++ app/configs/constants.js
@@ -1,4 +1,4 @@
 export default {
     API_URL: 'http://ku-4-0.herokuapp.com',
-    APP_ID: '199556151123995'
+    APP_ID: '518071865352584'
 }
--- app/services/user.js
+++ app/services/user.js
@@ -1,7 +1,7 @@
-import constants from '../configs/constants';
-import axios from 'axios';
-import * as Facebook from 'expo-facebook';
-import * as Google from 'expo-google-app-auth';
+import constants from '../configs/constants'
+import axios from 'axios'
+import * as Facebook from 'expo-facebook'
+import * as Google from 'expo-google-app-auth'
 
 export default {
     login: (username, password) => {
@@ -16,52 +16,47 @@ export default {
             .then(response => response.data)
             .catch(error => error)
     },
-    getProfile: ()=>{
+    getProfile: () => {
         return axios.get(`${constants.API_URL}/profile`)
     },
-    getUserById: (id)=>{
+    getUserById: (id) => {
         return axios.get(`${constants.API_URL}/users/${id}`).then(response => response.data)
-        .catch(error => error)
+            .catch(error => error)
     },
     loginByFacebook: async () => {
-        const appId = constants.APP_ID;
-        const permissions = ['public_profile', 'email'];
-        await Facebook.initializeAsync(appId);
-        const {
-            type,
-            token,
-        } = await Facebook.logInWithReadPermissionsAsync(
-            appId,
-            { permissions }
-        );
-        switch (type) {
-            case 'success': {
-                const response = await axios.get(`https://graph.facebook.com/me?access_token=${token}`);
-                console.log('Logged in!', `Hi ${(response.data.name)}!`);
-                return response.data
+        const app_id = constants.APP_ID
+        const permissions = ['public_profile', 'email']
+        try {
+            await Facebook.initializeAsync(app_id)
+            const {
+                type,
+                token,
+            } = await Facebook.logInWithReadPermissionsAsync(
+                app_id,
+                { permissions }
+            )
+            if (type === 'success') {
+                const response = await axios.get(`https://graph.facebook.com/me?access_token=${token}`)
+                return Promise.resolve(response.data)
+            } else {
+                return Promise.reject('Cancel by user')
             }
-            case 'cancel': {
-                return Promise.reject('Cancel by user');
-            }
+        } catch ({ message }) {
+            Promise.reject(`Facebook Login Error: ${message}`)
         }
     },
     loginByGoogle: async () => {
-
-        const result = await Google.logInAsync({
+        const { type, accessToken, user } = await Google.logInAsync({
             androidClientId: '319434606205-n3k9ijd91al3h7bcropp4e17rf8j1klk.apps.googleusercontent.com',
             iosClientId: '319434606205-c5ooi2joi9d4un5pi227aitkpls5ku10.apps.googleusercontent.com',
             scopes: ['profile', 'email'],
-            // iosStandaloneAppClientId: `319434606205-c5ooi2joi9d4un5pi227aitkpls5ku10.apps.googleusercontent.com`,
-            // androidStandaloneAppClientId: `319434606205-n3k9ijd91al3h7bcropp4e17rf8j1klk.apps.googleusercontent.com`,
         }
-        );
-
-        if (result.type === 'success') {
-            return result.user
+        )
+        if (type === 'success') {
+            return Promise.resolve(user)
         }
         else {
-            return Promise.reject('Cancel by user');
+            return Promise.reject('Cancel by user')
         }
     }
-
 }
