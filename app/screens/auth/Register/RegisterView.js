7bf2db10772adb2647d57078cf72a4ab47551f80
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-02-25 17:02:18 +0700

    Created passwordConfirm attribute & Created passwordMatch method & Imported register from props

--- app/screens/auth/Register/RegisterView.js
+++ app/screens/auth/Register/RegisterView.js
@@ -15,6 +15,7 @@ class RegisterView extends React.Component {
         this.state = {
             username: '',
             password: '',
+            passwordConfirm: '',
             isHide: true
         }
     }
@@ -49,9 +50,13 @@ class RegisterView extends React.Component {
             : <Text style={styles.textButton}>ลงทะเบียน</Text>
     }
 
+    passwordMatch(password, passwordConfirm) {
+        return (password === passwordConfirm)
+    }
+
     render() {
-        const { isHide, username, password } = this.state
-        const { login, loading, loginByFacebook, loginByGoogle } = this.props
+        const { isHide, username, password, passwordConfirm } = this.state
+        const { login, loading, loginByFacebook, loginByGoogle, register } = this.props
         return (
             <LinearGradient colors={[KU_PRIMARY_COLOR, KU_SECONDARY_COLOR]} style={styles.container} >
                 <View style={styles.innerContainer}>
@@ -94,12 +99,13 @@ class RegisterView extends React.Component {
                                 placeholder='ยืนยันรหัสผ่าน'
                                 placeholderTextColor='white'
                                 secureTextEntry={isHide}
-                                onChangeText={(text) => this.setState({ password: text })}
+                                onChangeText={(text) => this.setState({ passwordConfirm: text })}
                                 value={this.state.text}>
                             </TextInput>
                         </View>
                         <Button rounded style={styles.buttonContainer} disabled={loading} onPress={() => {
-                            login(username, password)
+                            if(this.passwordMatch(password, passwordConfirm)) register(username, password)
+                            else AlertHelper.alert('error', 'เกิดข้อผิดพลาด', 'รหัสผ่านไม่ตรงกัน')
                         }}>
                             {this.renderButton()}
                         </Button>
