1c35dd01aafab4c20bfc15cb84724f951b4f9d10
kakmond <wongsathorn.pa@ku.th>
2020-03-11 15:28:50 +0700

    removed section header

--- app/screens/News/Promotion/PromotionView.js
+++ app/screens/News/Promotion/PromotionView.js
@@ -3,15 +3,6 @@ import { ScrollView, View } from 'react-native';
 import styles from './styles';
 import NewsCard from '../../../components/news/NewsThread'
 import newsService from '../../../services/news'
-import SliderBox from '../../../components/news/PaginationSlider';
-import SectionHeader from '../../../components/commons/SectionHeader'
-
-const images = [
-    'https://cdn.majorcineplex.com/uploads/content/12694/cover_12694.jpg',
-    'https://www.ku.ac.th/web2012/resources/upload/content/images/symbol_KU62.png',
-    'https://www.mannaturecoconutmilk.com/uploads/110862coconutmilktabletforcowsmilkallergy.jpg',
-    'https://i.ebayimg.com/images/g/vuoAAOSw~M5cWv1Y/s-l1600.jpg',
-]
 
 class PromotionView extends React.Component {
 
@@ -60,15 +51,6 @@ class PromotionView extends React.Component {
         const { news } = this.state
         return (
             <ScrollView contentContainerStyle={styles.containter}>
-                <SectionHeader style={{ margin: 10 }} title={'กำลังฮิต'} subtitle={'เพิ่มเติม'} />
-                <SliderBox
-                    sliderBoxHeight={200}
-                    data={images}
-                    onPressed={id =>
-                        this.props.navigation.navigate('Detail', { id })
-                    }
-                />
-                <SectionHeader style={{ margin: 10 }} title={'ข่าวล่าสุด'} subtitle={'เพิ่มเติม'} />
                 {news.map((news, index, newsArray) => {
                     return (
                         <View key={news.newsId} style={index != 0 ? { marginTop: 10, backgroundColor: 'white' } : { backgroundColor: 'white' }}>
