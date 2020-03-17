bfe12b15be30e692030523cd776a460c577bd7f0
puow-mond <wongsathorn.pa@ku.th>
2020-03-17 21:19:59 +0700

    deleted community tag reducer

--- app/components/community/CommunityTab/CommunityTabContainer.js
+++ app/components/community/CommunityTab/CommunityTabContainer.js
@@ -1,17 +1,14 @@
 import { connect } from 'react-redux';
 import CommunityTabView from './CommunityTabView';
-import { selectCommunityTag } from '../../../reducers/CommunityTagReducer/actions';
 
 const mapStateToProps = state => {
     return {
-        selectedTag: state.communityTagReducer.selectedTag,
-        loading: state.communityTagReducer.loading,
-        error: state.communityTagReducer.error,
+
     }
 };
 
 const mapDispatchToProps = {
-    selectCommunityTag
+
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommunityTabView);
