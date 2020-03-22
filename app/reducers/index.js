925f50d9cd5cc52dea808b4603b5410e01bb6ce9
puow-mond <wongsathorn.pa@ku.th>
2020-03-22 23:36:52 +0700

    added searchReducer in combineReducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './UserReducer'
+import searchReducer from './SearchReducer'
 
 export default combineReducers({
     userReducer,
+    searchReducer
 });
