c7e7fe11ce4dce255c453deaca19d9ed64da6528
puow-mond <wongsathorn.pa@ku.th>
2020-04-11 00:22:45 +0700

    changed value of status bar height

--- app/components/commons/SafeAreaView/styles.js
+++ app/components/commons/SafeAreaView/styles.js
@@ -1,10 +1,10 @@
-import { StyleSheet, Platform } from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet, Platform, StatusBar } from 'react-native'
+// import Constants from 'expo-constants'
 
 export default StyleSheet.create({
     topContainer: {
         flex: 0,
-        paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
         backgroundColor: '#465859'
     },
     bottomContainer: {
