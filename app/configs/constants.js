9cbcbf3daa595a92c0693e5754b85bea7bf320a9
puow-mond <wongsathorn.pa@ku.th>
2020-04-13 19:12:50 +0700

    added tags constant

--- app/configs/constants.js
+++ app/configs/constants.js
@@ -1,4 +1,38 @@
+import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons'
+
 export default {
     API_URL: 'https://uninews-ku.appspot.com',
-    APP_ID: '518071865352584'
+    APP_ID: '518071865352584',
+    TAGS: [
+        {
+            outlineIconName: 'commenting-o',
+            iconName: 'commenting',
+            text: 'ทั่วไป',
+            iconComponent: FontAwesome
+        },
+        {
+            outlineIconName: 'heart-o',
+            iconName: 'heart',
+            text: 'ความรัก',
+            iconComponent: FontAwesome
+        },
+        {
+            outlineIconName: 'lightbulb-o',
+            iconName: 'lightbulb-o',
+            text: 'การเรียน',
+            iconComponent: FontAwesome
+        },
+        {
+            outlineIconName: 'futbol-o',
+            iconName: 'futbol-o',
+            text: 'กีฬา',
+            iconComponent: FontAwesome
+        },
+        {
+            outlineIconName: 'alert-circle-outline',
+            iconName: 'alert-circle',
+            text: 'เตือนภัย',
+            iconComponent: MaterialCommunityIcons
+        },
+    ]
 }
