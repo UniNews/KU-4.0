c33b7d02297c2d0f4274a3a8abcbac15d95a1f6b
puow-mond <wongsathorn.pa@ku.th>
2020-04-25 14:35:20 +0700

    added KeyboardShift component

---
+++ app/components/commons/KeyboardShift/index.js
@@ -0,0 +1,71 @@
+import { PropTypes } from 'prop-types'
+import React, { Component } from 'react'
+import { Animated, Dimensions, Keyboard, TextInput, UIManager } from 'react-native'
+import styles from './styles'
+import { STATUS_BAR_HEIGHT } from '../../../assets/css/device'
+
+const { State: TextInputState } = TextInput
+
+export default class KeyboardShift extends Component {
+
+    state = {
+        shift: new Animated.Value(0),
+    }
+
+    componentDidMount() {
+        this.keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', this.handleKeyboardDidShow)
+        this.keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide)
+    }
+
+    componentWillUnmount() {
+        this.keyboardDidShowSub.remove()
+        this.keyboardDidHideSub.remove()
+    }
+
+    render() {
+        const { children } = this.props
+        const { shift } = this.state
+        return (
+            <Animated.View style={[styles.container, { transform: [{ translateY: shift }] }]}>
+                {children}
+            </Animated.View>
+        )
+    }
+
+    handleKeyboardDidShow = (event) => {
+        const { height: windowHeight } = Dimensions.get('window')
+        const keyboardHeight = event.endCoordinates.height
+        const currentlyFocusedField = TextInputState.currentlyFocusedField()
+        UIManager.measure(currentlyFocusedField, (originX, originY, width, height, pageX, pageY) => {
+            const fieldHeight = height
+            const fieldTop = pageY
+            const gap = (windowHeight - keyboardHeight) - (fieldTop + fieldHeight) - STATUS_BAR_HEIGHT
+            if (gap >= 0)
+                return
+            Animated.timing(
+                this.state.shift,
+                {
+                    toValue: gap,
+                    duration: 100,
+                    useNativeDriver: true,
+                }
+            ).start()
+        })
+    }
+
+    handleKeyboardDidHide = () => {
+        Animated.timing(
+            this.state.shift,
+            {
+                toValue: 0,
+                duration: 100,
+                useNativeDriver: true,
+            }
+        ).start()
+    }
+}
+
+
+KeyboardShift.propTypes = {
+    children: PropTypes.func.isRequired,
+}
---
+++ app/components/commons/KeyboardShift/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        height: '100%',
+        left: 0,
+        position: 'absolute',
+        top: 0,
+        width: '100%'
+    }
+})
