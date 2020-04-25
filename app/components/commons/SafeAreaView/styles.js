b5f88ea42bf4a33c09671697aad94fda157f319d
puow-mond <wongsathorn.pa@ku.th>
2020-04-25 19:47:28 +0700

    added status bar for android

--- app/components/commons/SafeAreaView/styles.js
+++ app/components/commons/SafeAreaView/styles.js
@@ -4,10 +4,11 @@ import * as Device from 'expo-device'
 export default StyleSheet.create({
     topContainer: {
         flex: 0,
-        paddingTop: Platform.OS === 'android' && Device.brand !== 'HUAWEI' ? StatusBar.currentHeight : 0,
+        // paddingTop: Platform.OS === 'android' && Device.brand !== 'HUAWEI' ? StatusBar.currentHeight : 0,
         backgroundColor: '#465859'
     },
     bottomContainer: {
-        flex: 1
+        flex: 1,
+        // backgroundColor: '#465859'
     }
 })
